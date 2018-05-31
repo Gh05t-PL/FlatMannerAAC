c57954c5d96b72ac7b547c8ebd383549ac93a614
Gh05t-PL <gozetero@gmail.com>
2018-05-31 15:39:45 +0200

    edit

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -62,7 +62,7 @@ class PlayersController extends Controller
                 $player->guild = $this->getDoctrine()->getManager()
                     ->createNativeQuery("SELECT guild_name,rank_name,guildId FROM players t3 INNER JOIN ( SELECT t2.name as guild_name, t2.id as guildId, t1.name as rank_name, t1.id as rankID FROM guild_ranks t1 INNER JOIN (SELECT * FROM guilds) t2 ON t1.guild_id = t2.id) t4 ON t3.rank_id = t4.rankID WHERE id = {$player->getId()}", $rsm)
                 ->getScalarResult();
-                \var_dump($player->guild);
+                
             }else{
                 $player->guild = "No Membership";
             }
