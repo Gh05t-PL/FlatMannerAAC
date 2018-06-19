3a6458f9a9d1b00e062970e8521ec600468c97d6
Gh05t-PL <gozetero@gmail.com>
2018-06-19 12:45:01 +0200

    edit
    added editing/removing of news when logged into admin account

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -79,6 +79,105 @@ class AdminPanelController extends Controller
 
 
     /**
+     * @Route("/admin/edit/article/{id}", name="adminPanel_edit_article", requirements={"id"="\d+"})
+     */
+    public function editArticle($id, SessionInterface $session, Request $request)
+    {
+        // check if XHR post request is pending
+        if ( $request->request->get('text') !== NULL && $request->request->get('title') !== NULL && $request->request->get('author') !== NULL){
+            // NOT LOGGED IN REDIR
+            if ($session->get('account_id') === NULL)
+                return new Response("Login first");
+
+            $account = $this->getDoctrine()
+                ->getRepository(\App\Entity\Accounts::class)
+            ->find($session->get('account_id'));
+
+
+            // CHECK IF ACCOUNT HAVE ADMIN PRIVILEGES
+            if ($account->getGroupId() < 7)
+                return new Response("No admin privileges");
+
+
+
+
+            $player = $this->getDoctrine()
+                ->getRepository(\App\Entity\Players::class)
+            ->findOneBy(['name' => $request->request->get('author')]);
+
+            if ( $player === NULL )
+                return new Response("Error author with name: '".$request->request->get('author')."' not found",Response::HTTP_NOT_FOUND);
+
+            $article = $this->getDoctrine()
+                ->getRepository(\App\Entity\FmaacNews::class)
+            ->find($id);
+
+            $article->setTitle($request->request->get('title'))
+                ->setText($request->request->get('text'))
+            ->setAuthor($player);
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($article);
+            $em->flush();
+
+            return new Response("Article with title: " . $request->request->get('title') . " | Edited");
+        }
+
+        // NOT LOGGED IN REDIR
+        if ($session->get('account_id') === NULL)
+            return $this->redirectToRoute('account_login', [], 301);
+
+        $account = $this->getDoctrine()
+            ->getRepository(\App\Entity\Accounts::class)
+        ->find($session->get('account_id'));
+
+        // CHECK IF ACCOUNT HAVE ADMIN PRIVILEGES
+        if ($account->getGroupId() < 7)
+            return $this->redirectToRoute('account', [], 301);
+
+
+        $article = $this->getDoctrine()
+            ->getRepository(\App\Entity\FmaacNews::class)
+        ->find($id);
+
+        return $this->render('admin_panel/edit_article.html.twig', [
+            'controller_name' => 'NewsController',
+            'article' => $article,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/delete/article/{id}", name="adminPanel_delete_article", requirements={"id"="\d+"})
+     */
+    public function deleteArticle($id, SessionInterface $session, Request $request)
+    {
+        // NOT LOGGED IN REDIR
+        if ($session->get('account_id') === NULL)
+            return $this->redirectToRoute('account_login', [], 301);
+
+        $account = $this->getDoctrine()
+            ->getRepository(\App\Entity\Accounts::class)
+        ->find($session->get('account_id'));
+
+        // CHECK IF ACCOUNT HAVE ADMIN PRIVILEGES
+        if ($account->getGroupId() < 7)
+            return $this->redirectToRoute('account', [], 301);
+
+
+        $article = $this->getDoctrine()
+            ->getRepository(\App\Entity\FmaacNews::class)
+        ->find($id);
+
+        $em = $this->getDoctrine()->getManager();
+
+        $em->remove($article);
+        $em->flush();
+        return $this->redirectToRoute('news', [], 301);
+    }
+
+
+    /**
      * @Route("/admin", name="adminPanel")
      */
     public function index(SessionInterface $session)
--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -6,8 +6,10 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
+
 use App\Entity\Players;
 use App\Entity\FmaacNews;
+
 
 class NewsController extends Controller
 {
@@ -26,7 +28,7 @@ class NewsController extends Controller
     /**
      * @Route("/news/{page}", name="news", requirements={"page"="\d+"})
      */
-    public function news($page = 1)
+    public function news($page = 1, SessionInterface $session)
     {
         $newsCount = count($this->getDoctrine()
             ->getRepository(FmaacNews::class)
@@ -36,23 +38,32 @@ class NewsController extends Controller
         $resultsLimit = 5;
         $pagesCount = ceil(($newsCount / $resultsLimit));
 
+        $isAdmin = false;
+        if ( $session->get('account_id') !== NULL ){
+            $account = $this->getDoctrine()
+                ->getRepository(\App\Entity\Accounts::class)
+            ->find($session->get('account_id'));
+
+            if ( $account->getGroupId() >= 7 )
+                $isAdmin = true;
+        }
+                
+
+            
 
         $query = $this->getDoctrine()
             ->getManager()
         ->createQuery("SELECT u FROM App\Entity\FmaacNews u ORDER BY u.datetime DESC")->setMaxResults($resultsLimit)->setFirstResult($resultsLimit*($page-1));
         $news = $query->getResult();
-        //var_dump($news);
-
-
 
 
 
 
         return $this->render('news/news.html.twig', [
-            'controller_name' => 'NewsController',
             'page' => $page,
             'news' => $news,
             'pagesCount' => $pagesCount,
+            'isAdmin' => $isAdmin,
         ]);
     }
 
@@ -60,7 +71,7 @@ class NewsController extends Controller
     /**
      * @Route("/article/{id}", name="article", requirements={"id"="\d+"})
      */
-    public function article($id = 1)
+    public function article($id = 1, SessionInterface $session)
     {
 
         $article = $this->getDoctrine()
@@ -68,10 +79,20 @@ class NewsController extends Controller
         ->find($id);
 
 
+        $isAdmin = false;
+        if ( $session->get('account_id') !== NULL ){
+            $account = $this->getDoctrine()
+                ->getRepository(\App\Entity\Accounts::class)
+            ->find($session->get('account_id'));
+
+            if ( $account->getGroupId() >= 7 )
+                $isAdmin = true;
+        }
+
 
         return $this->render('news/article.html.twig', [
-            'controller_name' => 'NewsController',
             'article' => $article,
+            'isAdmin' => $isAdmin,
         ]);
     }
 }
